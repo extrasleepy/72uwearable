30e2753fbbcecad38f69e763b047d5438099109b
Andrew Kleindolph <akleindolph@gmail.com>
2016-06-30 16:53:38 -0700

    serial updates

--- light_only/light_only.ino
+++ light_only/light_only.ino
@@ -6,13 +6,13 @@ void setup() {
   CircuitPlayground.begin();     // Setup Circuit Playground library.
 
   Serial.begin(9600);     // Setup serial port.
-  Serial.println("Light Sensor Raw Value");
 }
 
 void loop() {
   
   // Get the sensor sensor value and print it to Serial Monitor
   uint16_t value = analogRead(ANALOG_INPUT);
+  Serial.print("raw lightlevel= ");
   Serial.println(value, DEC);
 
   delay(500); // half second delay between readings
--- sound_only/sound_only.ino
+++ sound_only/sound_only.ino
@@ -1,19 +1,18 @@
 #include <Adafruit_CircuitPlayground.h>   //required library to use module
 
-#define ANALOG_INPUT A0  //Analog 0 is connected to temperature sensor
+#define ANALOG_INPUT A4  //Analog 4 is connected to sound sensor/microphone
 
 void setup() {
   CircuitPlayground.begin();     // Setup Circuit Playground library.
 
   Serial.begin(9600);     // Setup serial port.
-
 }
 
 void loop() {
   
   // Get the sensor sensor value and print it to Serial Monitor
   uint16_t value = analogRead(ANALOG_INPUT);
-  Serial.print("raw templevel= ");
+  Serial.print("raw soundlevel= ");
   Serial.println(value, DEC);
 
   delay(500); // half second delay between readings
