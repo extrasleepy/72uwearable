0b9af5d23a26d4949d80c62288a5b73307f34014
Andrew Kleindolph <akleindolph@gmail.com>
2016-06-30 16:45:55 -0700

    first commit
    Super simple circuit playground light reading example

---
+++ light_only/light_only.ino
@@ -0,0 +1,20 @@
+#include <Adafruit_CircuitPlayground.h>   //required library to use module
+
+#define ANALOG_INPUT A5  //Analog 5 is connected to light sensor
+
+void setup() {
+  CircuitPlayground.begin();     // Setup Circuit Playground library.
+
+  Serial.begin(9600);     // Setup serial port.
+  Serial.println("Light Sensor Raw Value");
+}
+
+void loop() {
+  
+  // Get the sensor sensor value and print it to Serial Monitor
+  uint16_t value = analogRead(ANALOG_INPUT);
+  Serial.println(value, DEC);
+
+  delay(500); // half second delay between readings
+
+}
