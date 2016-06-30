d5f086952206692691d203dde75a121e4a239552
Andrew Kleindolph <akleindolph@gmail.com>
2016-06-30 16:40:55 -0700

    first commit

--- temp_only/temp_only.ino
+++ temp_only/temp_only.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Adafruit_CircuitPlayground.h>   //required library to use module
 
+#define ANALOG_INPUT A0  //Analog 5 is connected to temperature sensor
+
+void setup() {
+  CircuitPlayground.begin();     // Setup Circuit Playground library.
+
+  Serial.begin(9600);     // Setup serial port.
+  Serial.println("Temperaure Sensor Raw Value");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  // Get the sensor sensor value and print it to Serial Monitor
+  uint16_t value = analogRead(ANALOG_INPUT);
+  Serial.println(value, DEC);
+
+  delay(500); // half second delay between readings
 
 }
