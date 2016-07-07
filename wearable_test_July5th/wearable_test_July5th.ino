1c87ea9dcef5d1a102631739ff2f4e4d88432a9b
Andrew Kleindolph <akleindolph@gmail.com>
2016-07-06 22:15:12 -0700

    best version + experimenting with tones

--- wearable_test_July5th/wearable_test_July5th.ino
+++ wearable_test_July5th/wearable_test_July5th.ino
@@ -1,21 +1,24 @@
+//Code for 72u wearable electroincs project. Summer 2016
+
 #include <Adafruit_CircuitPlayground.h>   //required library to use module
+#include <Narcoleptic.h>  //library that allows low power sleeping
 
 #define TEMP A0  //Analog 0 is connected to temperature sensor
 #define SOUND A4  //Analog 4 is connected to sound sensor/microphone
 #define LIGHT A5  //Analog 5 is connected to light sensor
 
-float tMin = 510;   //min max variables for sensors
+#define TONE_DURATION_MS 100  // Duration in milliseconds to play a tone when touched.
+
+float tMin = 510;   //min max variables for sensors   < - - - these are currently a little arbitrary
 float tMax = 580;
 float sMin = 300;
 float sMax = 400;
 float lMin = 900;
 float lMax = 1100;
 
-bool tempRange = false;
-bool soundRange = false;
-bool lightRange = false;
-
-int pixels[] = {0, 2, 4, 5, 7, 9};   //array of used pins
+int pixels[] = {0, 2, 4, 5, 7, 9};   //array of used light pins
+int tones[] = {300, 500, 580};   //array for adding sound
+int tlength[] = {200, 250, 400};  //array for adding sound durations
 
 void setup() {
   CircuitPlayground.begin();     // Setup Circuit Playground library.
@@ -24,11 +27,11 @@ void setup() {
 }
 
 void loop() {
-
-  // Get the sensor sensor values and print it to Serial Monitor
+  // Get the sensor sensor values
   uint16_t tempValue = analogRead(TEMP);
   uint16_t soundValue = analogRead(SOUND);
   uint16_t lightValue = analogRead(LIGHT);
+  //print sensor values to serial monitor  < --- comment out to save power
   Serial.print("raw temp= ");
   Serial.println(tempValue, DEC);
   Serial.print("raw sound= ");
@@ -36,14 +39,23 @@ void loop() {
   Serial.print("raw light= ");
   Serial.println(lightValue, DEC);
 
-  lightUp(tempValue, soundValue, lightValue);
-  delay(1000); // 1 second delay between readings
-
+  lightUp(tempValue, soundValue, lightValue);   //function to lights fades
+  delay(5000);
+  //Narcoleptic.delay(5000); // 5 second sleep between readings <----bring in later
 }
 
 
 uint16_t lightUp(uint16_t tempValue, uint16_t soundValue, uint16_t lightValue) {
 
+  //play tone if environment is perfect   <----causing clicking??
+  if (tempValue > tMin && tempValue < tMax && soundValue > sMin && soundValue < sMax && lightValue > lMin && lightValue < lMax) {
+    for (int i = 0; i < sizeof(tones); i++) {
+      CircuitPlayground.playTone(tones[i], tlength[i]);
+      delay(tlength[i]);
+    }
+  }
+
+  //fade in loop
   for (int fd = 0; fd < 255; fd++) {
     if (tempValue > tMin && tempValue < tMax && soundValue > sMin && soundValue < sMax && lightValue > lMin && lightValue < lMax) {
       for (int i = 0; i < 7; i++) {
@@ -67,8 +79,9 @@ uint16_t lightUp(uint16_t tempValue, uint16_t soundValue, uint16_t lightValue) {
     }
     CircuitPlayground.strip.show();  // update pixels!
     delay(5);
-
   }
+
+  //fade out loop
   for (int fd = 255; fd > 0; fd--) {
     if (tempValue > tMin && tempValue < tMax && soundValue > sMin && soundValue < sMax && lightValue > lMin && lightValue < lMax) {
       for (int i = 0; i < 7; i++) {
@@ -93,9 +106,5 @@ uint16_t lightUp(uint16_t tempValue, uint16_t soundValue, uint16_t lightValue) {
     CircuitPlayground.strip.show();  // update pixels!
     delay(5);
   }
-
-
-
-
 }
 
