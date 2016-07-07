b5a8c8dadf383f684926133613461e79a4f10cd1
Andrew Kleindolph <akleindolph@gmail.com>
2016-07-06 21:05:18 -0700

    Best version so far
    fades lights based on sensors 1 time every second.

--- wearable_test_July5th/wearable_test_July5th.ino
+++ wearable_test_July5th/wearable_test_July5th.ino
@@ -36,26 +36,66 @@ void loop() {
   Serial.print("raw light= ");
   Serial.println(lightValue, DEC);
 
-  checkValues(tempValue, soundValue, lightValue);
-  CircuitPlayground.strip.show();  // update pixels!
-  delay(100); // 10th second delay between readings
+  lightUp(tempValue, soundValue, lightValue);
+  delay(1000); // 1 second delay between readings
 
-  for (int i = 0; i < 10; i++) {   //turn off pixels (only visible if above parameters not met)
-    CircuitPlayground.strip.setPixelColor(i, 0, 0, 0);
-  }
 }
 
-uint16_t checkValues(uint16_t tempValue, uint16_t soundValue, uint16_t lightValue) {
-  if (tempValue > tMin && tempValue < tMax) {     //see if temp is within range
-    tempRange=true;
+
+uint16_t lightUp(uint16_t tempValue, uint16_t soundValue, uint16_t lightValue) {
+
+  for (int fd = 0; fd < 255; fd++) {
+    if (tempValue > tMin && tempValue < tMax && soundValue > sMin && soundValue < sMax && lightValue > lMin && lightValue < lMax) {
+      for (int i = 0; i < 7; i++) {
+        CircuitPlayground.strip.setPixelColor(pixels[i], fd, fd, fd);   //white
+        CircuitPlayground.strip.show();  // update pixels!
+      }
+    }
+    else {
+      if (tempValue > tMin && tempValue < tMax) {
+        CircuitPlayground.strip.setPixelColor(0, fd, 0, fd);   //magenta (temp)
+        CircuitPlayground.strip.setPixelColor(9, fd, 0, fd);   //magenta (temp)
+      }
+      if (soundValue > sMin && soundValue < sMax) {
+        CircuitPlayground.strip.setPixelColor(2, fd, fd, 0);    //yellow (sound)
+        CircuitPlayground.strip.setPixelColor(4, fd, fd, 0);    //yellow (sound)
+      }
+      if (lightValue > lMin && lightValue < lMax) {
+        CircuitPlayground.strip.setPixelColor(5, 0, fd, fd);     //cyan (light)
+        CircuitPlayground.strip.setPixelColor(7, 0, fd, fd);     //cyan (light)
+      }
+    }
+    CircuitPlayground.strip.show();  // update pixels!
+    delay(5);
+
+  }
+  for (int fd = 255; fd > 0; fd--) {
+    if (tempValue > tMin && tempValue < tMax && soundValue > sMin && soundValue < sMax && lightValue > lMin && lightValue < lMax) {
+      for (int i = 0; i < 7; i++) {
+        CircuitPlayground.strip.setPixelColor(pixels[i], fd, fd, fd);   //white
+        CircuitPlayground.strip.show();  // update pixels!
+      }
+    }
+    else {
+      if (tempValue > tMin && tempValue < tMax) {
+        CircuitPlayground.strip.setPixelColor(0, fd, 0, fd);   //magenta (temp)
+        CircuitPlayground.strip.setPixelColor(9, fd, 0, fd);   //magenta (temp)
+      }
+      if (soundValue > sMin && soundValue < sMax) {
+        CircuitPlayground.strip.setPixelColor(2, fd, fd, 0);    //yellow (sound)
+        CircuitPlayground.strip.setPixelColor(4, fd, fd, 0);    //yellow (sound)
+      }
+      if (lightValue > lMin && lightValue < lMax) {
+        CircuitPlayground.strip.setPixelColor(5, 0, fd, fd);     //cyan (light)
+        CircuitPlayground.strip.setPixelColor(7, 0, fd, fd);     //cyan (light)
+      }
+    }
+    CircuitPlayground.strip.show();  // update pixels!
+    delay(5);
   }
 
-  if (soundValue > sMin && soundValue < sMax) {    //see if sound is within range
-     soundRange=true;
-  }
 
-  if (lightValue > lMin && lightValue < lMax) {   //see if light is within range
-     lightRange=true;
-  }
+
+
 }
 
