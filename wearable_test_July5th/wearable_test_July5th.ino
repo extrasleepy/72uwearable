91af576bf63c85f15b07c1df7c3fb8ba11183cb6
Andrew Kleindolph <akleindolph@gmail.com>
2016-07-07 22:40:58 -0700

    colors switched and light spin added

--- wearable_test_July5th/wearable_test_July5th.ino
+++ wearable_test_July5th/wearable_test_July5th.ino
@@ -1,15 +1,15 @@
 //Code for 72u wearable electroincs project. Summer 2016
 
 /* TODO::
-   ->White lights spin 2 times when all parameters are correct
-   ->What's the final color when all parameters are met
+   DONE: White lights spin 2 times when all parameters are correct BUT ONLY ONCE PER SESSION
+   ->What's the final color when all parameters are met??
    ->Make cool tones (JAVI working on this)
    ->Perfect environment tone should only play once per 4 hours? even if you temporarily exit perfect environment. If you don't leave perfect environment, it won't go off ever again.
-   ->How often do we see the lights? once every 60 sec
+   DONE: (needs to be uncommented) How often do we see the lights? once every 60 sec
    ->Go into deep sleep when no movement is detected over how long + wake up on accelerometer movement
    ->Touch "remember an idea" sequence  - - reminder after 2 hours.
    ->We need real numbers on the sensor min/max values (JAVI working on this)
-   ->light should change to yellow, temp should be cyan, magenta sound 
+   DONE: light should change to yellow, temp should be cyan, magenta sound
 */
 
 #include <Adafruit_CircuitPlayground.h>   //required library to use module
@@ -30,7 +30,9 @@ float lMax = 1100;
 
 int pixels[] = {0, 2, 4, 5, 7, 9};   //array of used light pins
 int tones[] = {100, 500, 1000};   //array for adding sound
-int tlength[] = {200, 250, 1000};  //array for adding sound durations
+int tlength[] = {200, 250, 200};  //array for adding sound durations
+
+bool resetSpin = true;
 
 void setup() {
   CircuitPlayground.begin();     // Setup Circuit Playground library.
@@ -52,19 +54,38 @@ void loop() {
   Serial.println(lightValue, DEC);
 
   lightUp(tempValue, soundValue, lightValue);   //function to lights fades
-  delay(5000);    //eventually 60 sec
-  //Narcoleptic.delay(5000); // 5 second sleep between readings <----bring in later
+  delay(5000);    // eventually 60 sec
+  //Narcoleptic.delay(60000); // 60 second sleep between readings <----bring in later
 }
 
 
 uint16_t lightUp(uint16_t tempValue, uint16_t soundValue, uint16_t lightValue) {
 
   //play tone if environment is perfect   <----causing clicking??
-  if (tempValue > tMin && tempValue < tMax && soundValue > sMin && soundValue < sMax && lightValue > lMin && lightValue < lMax) {
-    for (int i = 0; i < sizeof(tones); i++) {
+  if (tempValue > tMin && tempValue < tMax && soundValue > sMin && soundValue < sMax && lightValue > lMin && lightValue < lMax && resetSpin==true) {
+    for (int i = 0; i <= sizeof(tones-1); i++) {
       CircuitPlayground.playTone(tones[i], tlength[i]);
       delay(tlength[i]);
     }
+    
+    //spin twice
+    for (int spin = 0; spin < 3; spin++) {   //spin happens twice
+      for (int i = 0; i < 10; i++) {    //fade each of the 6 lights
+        for (int fd = 0; fd <= 255; fd+=3) {
+          CircuitPlayground.strip.setPixelColor(i, fd, fd, fd);   //white
+          CircuitPlayground.strip.setPixelColor(9-i, fd, fd, fd);   //white
+          CircuitPlayground.strip.show();  // update pixels!
+          delayMicroseconds(1);
+        }
+        for (int fd = 255; fd >= 0; fd-=3) {
+          CircuitPlayground.strip.setPixelColor(i, fd, fd, fd);   //white
+          CircuitPlayground.strip.setPixelColor(9-i, fd, fd, fd);   //white
+          CircuitPlayground.strip.show();  // update pixels!
+          delayMicroseconds(1);
+        }
+      }
+    }
+    resetSpin=false;
   }
 
   //fade in loop
@@ -76,17 +97,21 @@ uint16_t lightUp(uint16_t tempValue, uint16_t soundValue, uint16_t lightValue) {
       }
     }
     else {
+      resetSpin=true;
+      
       if (tempValue > tMin && tempValue < tMax) {
-        CircuitPlayground.strip.setPixelColor(0, fd, 0, fd);   //magenta (temp)
-        CircuitPlayground.strip.setPixelColor(9, fd, 0, fd);   //magenta (temp)
+        CircuitPlayground.strip.setPixelColor(5, 0, fd, fd);     //cyan (temp)
+        CircuitPlayground.strip.setPixelColor(7, 0, fd, fd);     //cyan (temp)
+
       }
       if (soundValue > sMin && soundValue < sMax) {
-        CircuitPlayground.strip.setPixelColor(2, fd, fd, 0);    //yellow (sound) 
-        CircuitPlayground.strip.setPixelColor(4, fd, fd, 0);    //yellow (sound)
+        CircuitPlayground.strip.setPixelColor(0, fd, 0, fd);   //magenta (sound)
+        CircuitPlayground.strip.setPixelColor(9, fd, 0, fd);   //magenta (sound)
+
       }
       if (lightValue > lMin && lightValue < lMax) {
-        CircuitPlayground.strip.setPixelColor(5, 0, fd, fd);     //cyan (light)
-        CircuitPlayground.strip.setPixelColor(7, 0, fd, fd);     //cyan (light)
+        CircuitPlayground.strip.setPixelColor(2, fd, fd, 0);    //yellow (light)
+        CircuitPlayground.strip.setPixelColor(4, fd, fd, 0);    //yellow (light)
       }
     }
     CircuitPlayground.strip.show();  // update pixels!
@@ -103,16 +128,16 @@ uint16_t lightUp(uint16_t tempValue, uint16_t soundValue, uint16_t lightValue) {
     }
     else {
       if (tempValue > tMin && tempValue < tMax) {
-        CircuitPlayground.strip.setPixelColor(0, fd, 0, fd);   //magenta (temp)
-        CircuitPlayground.strip.setPixelColor(9, fd, 0, fd);   //magenta (temp)
+        CircuitPlayground.strip.setPixelColor(5, 0, fd, fd);     //cyan (temp)
+        CircuitPlayground.strip.setPixelColor(7, 0, fd, fd);     //cyan (temp)
       }
       if (soundValue > sMin && soundValue < sMax) {
-        CircuitPlayground.strip.setPixelColor(2, fd, fd, 0);    //yellow (sound)
-        CircuitPlayground.strip.setPixelColor(4, fd, fd, 0);    //yellow (sound)
+        CircuitPlayground.strip.setPixelColor(0, fd, 0, fd);   //magenta (sound)
+        CircuitPlayground.strip.setPixelColor(9, fd, 0, fd);   //magenta (sound)
       }
       if (lightValue > lMin && lightValue < lMax) {
-        CircuitPlayground.strip.setPixelColor(5, 0, fd, fd);     //cyan (light)
-        CircuitPlayground.strip.setPixelColor(7, 0, fd, fd);     //cyan (light)
+        CircuitPlayground.strip.setPixelColor(2, fd, fd, 0);    //yellow (light)
+        CircuitPlayground.strip.setPixelColor(4, fd, fd, 0);    //yellow (light)
       }
     }
     CircuitPlayground.strip.show();  // update pixels!
