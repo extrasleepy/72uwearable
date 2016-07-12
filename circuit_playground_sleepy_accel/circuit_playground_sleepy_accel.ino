e1b81f47c1f63cbf3b474e207d1aa5db00f237c6
Andrew Kleindolph <akleindolph@gmail.com>
2016-07-12 09:59:41 -0700

    experimenting

--- circuit_playground_sleepy_accel/circuit_playground_sleepy_accel.ino
+++ circuit_playground_sleepy_accel/circuit_playground_sleepy_accel.ino
@@ -1,5 +1,12 @@
 #include <Adafruit_CircuitPlayground.h>   //required library to use module
-#include <Narcoleptic.h>
+//#include <Narcoleptic.h>
+
+int xPrevious = 0;
+int yPrevious = 0;
+int zPrevious = 0;
+int xMove = 0;
+int yMove = 0;
+int zMove = 0;
 
 void setup() {
   CircuitPlayground.begin();     // Setup Circuit Playground library.
@@ -12,17 +19,22 @@ void loop() {
   CircuitPlayground.strip.setPixelColor(1, 255, 255, 255);  //white
   CircuitPlayground.strip.show();  // update pixels!
 
-  uint8_t xMove = CircuitPlayground.motionX();
-  uint8_t yMove = CircuitPlayground.motionY();
-  uint8_t zMove = CircuitPlayground.motionZ();
-
+  xMove = CircuitPlayground.motionX();
+  yMove = CircuitPlayground.motionY();
+  zMove = CircuitPlayground.motionZ();
   Serial.println(xMove);
-  Serial.println(yMove);
-  Serial.println(zMove);
+  // Serial.println(yMove);
+  //Serial.println(zMove);
 
-  if (xMove == 0) {
+  if (xMove == xPrevious) {
     Serial.println("not moving");
   }
+
+  uint8_t xPrevious = xMove;
+  uint8_t yPrevious = yMove;
+  uint8_t zPrevious = zMove;
+
+  Serial.println(xPrevious);
 
   delay(100);
 
