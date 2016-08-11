d5086296b2e889494f120e6214b68724da6de525
Andrew Kleindolph <akleindolph@gmail.com>
2016-08-10 22:47:49 -0700

    sound improved, temp and light error fix, tone interval added

--- locu_final/locu_final.ino
+++ locu_final/locu_final.ino
@@ -7,17 +7,14 @@
 #define SOUND A4  //Analog 4 is connected to sound sensor/microphone
 #define LIGHT A5  //Analog 5 is connected to light sensor
 
-#define CAP_THRESHOLD    50  // Threshold for a capacitive touch (higher = less sensitive).
-#define CAP_SAMPLES      20   // Number of samples to take for a capacitive touch read.
-
 float tMin = 69;   //min max variables for sensors temp = 70-74 degrees, sound=300-550 based on db, light=20-180 based on 150 lux
-float tMax = 75;   //all these numbers could use some testing
-float sMin = 300;
+float tMax = 85;
+float sMin = 25;
 float sMax = 550;
 float lMin = 40;
 float lMax = 260;
 
-float tempCalib = 12; //variable to calibrate temperature
+float tempCalib = 8; //variable to calibrate temperature
 
 uint8_t xPrev = 0;  //variables to test movement for sleep mode
 uint8_t yPrev = 0;
@@ -28,20 +25,25 @@ uint8_t zMove = 0;
 
 int moveFlex = 2; //a little flexibility for minor vibrations
 int moveTimer = 0;  //timer keeps track how long since last movement
-int verySleepy = 5600; //1 represents about 1 second + total light fade times (5600 = 120ish min)
+int verySleepy = 2600; //1 represents about 1 second + total light fade times (2600 = 60ish min)
+int quietTimeInterval = 1300; //1 represents about 1 second + total light fade times (1300 = 30ish min)
+int quietTimer = 0; //timer keeps track how long since last tone
 
 int pixels[] = {0, 2, 4, 5, 7, 9};   //array of used light pins
 int perfectTones[] =       {300, 380, 440, 620};   //array for perfect environment sound
 int perfectTonesLength[] = {125, 125, 200, 300};  //array for adding sound durations
 
 bool resetSpin = true;  //keeps track of how often light spin happens
+bool quietTime = false; //keeps tone from playing too often
 
-long fadeInterval = 60000; // interval between light fades (60ish seconds)
-long sinceLastFade = 59000; //fades one time right away when powered up
+long fadeInterval = 30000; // interval between light fades (60ish seconds)
+long sinceLastFade = 29000; //fades one time right away when powered up
 
 uint16_t tempValue = 0;   //used to average sensors
-uint16_t soundValue = 0;
 uint16_t lightValue = 0;
+
+const int sampleWindow = 1000; // Sample window for mic.
+unsigned int sample;
 
 int fadeRate = 1500;
 int between = 15;
@@ -78,16 +80,36 @@ void loop() {
     uint16_t soundSample = 0;
     uint16_t lightSample = 0;  //used to average sensors
 
-    for (int i = 0; i < 20; i++) {  //take 10 samples over 1 second
-
+    for (int i = 0; i < 20; i++) {  //take 20 samples over 2 seconds
       tempSample += CircuitPlayground.temperatureF() - tempCalib;
-      soundSample += analogRead(SOUND);
       lightSample += analogRead(LIGHT);
       delay(100);
     }
     tempValue = tempSample / 20;   //average the 10 samples
-    soundValue = soundSample / 20;
     lightValue = lightSample / 20;
+
+    unsigned long startMillis = millis(); // Start of sample window
+    unsigned int soundValue = 0;   // peak-to-peak level
+
+    unsigned int signalMax = 0;
+    unsigned int signalMin = 1024;
+
+    while (millis() - startMillis < sampleWindow)
+    {
+      sample = analogRead(A4);
+      if (sample < 1024)  // toss out spurious readings
+      {
+        if (sample > signalMax)
+        {
+          signalMax = sample;  // save just the max levels
+        }
+        else if (sample < signalMin)
+        {
+          signalMin = sample;  // save just the min levels
+        }
+      }
+    }
+    soundValue = signalMax - signalMin;  // max - min = peak-peak amplitude
 
     //print sensor values to serial monitor
     Serial.print("raw temp= ");
@@ -103,6 +125,22 @@ void loop() {
       lightUp(tempValue, soundValue, lightValue);   //function to lights fades
     }
   }
+  
+  if (quietTime == true) {
+    quietTimer++;
+    Serial.print("qt=");
+    Serial.println(quietTimer);
+  } else {
+    Serial.print("qt=");
+    Serial.println(quietTimer);
+    Serial.println("tone will play");
+  }
+  if (quietTimer > quietTimeInterval) {
+    quietTime = false;
+    quietTimer = 0;
+    Serial.println("tone reset");
+  }
+  
 }
 
 //function to control lights and sound based on sensor readings
@@ -110,9 +148,13 @@ uint16_t lightUp(uint16_t tempValue, uint16_t soundValue, uint16_t lightValue) {
 
   //play tone if environment is perfect
   if (tempValue > tMin && tempValue < tMax && soundValue > sMin && soundValue < sMax && lightValue > lMin && lightValue < lMax && resetSpin == true) {
-    for (int i = 0; i < sizeof(perfectTones) / sizeof(int); i++) {
-      CircuitPlayground.playTone(perfectTones[i], perfectTonesLength[i]);
-      delay(perfectTonesLength[i]);
+
+    if (quietTime == false) {
+      for (int i = 0; i < sizeof(perfectTones) / sizeof(int); i++) {
+        CircuitPlayground.playTone(perfectTones[i], perfectTonesLength[i]);
+        delay(perfectTonesLength[i]);
+      }
+      quietTime = true;
     }
 
     //light spin twice
@@ -156,7 +198,7 @@ uint16_t lightUp(uint16_t tempValue, uint16_t soundValue, uint16_t lightValue) {
   } else {
     resetSpin = true;
     //-----temp and light--------
-    if (lightValue > tMin && lightValue < tMax && tempValue > sMin && tempValue < sMax) {
+    if (lightValue > lMin && lightValue < lMax && tempValue > sMin && tempValue < sMax) {
       for (int fd = 0; fd < 127; fd++) {
         CircuitPlayground.strip.setPixelColor(2, fd, fd, 0);     //yellow (light)
         CircuitPlayground.strip.setPixelColor(5, 0, fd, fd);     //cyan (temp)
@@ -233,7 +275,7 @@ uint16_t lightUp(uint16_t tempValue, uint16_t soundValue, uint16_t lightValue) {
       delay(between);
     }
     //-----light and sound--------
-    else if (lightValue > tMin && lightValue < tMax && soundValue > sMin && soundValue < sMax) {
+    else if (lightValue > lMin && lightValue < lMax && soundValue > sMin && soundValue < sMax) {
       for (int fd = 0; fd < 127; fd++) {
         CircuitPlayground.strip.setPixelColor(2, fd, fd, 0);     //yellow (light)
         CircuitPlayground.strip.setPixelColor(0, fd, 0 , fd);     //magenta (sound)
