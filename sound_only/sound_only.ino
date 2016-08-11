12b8e69445a9feef1f2d7b4ea7d7ee721f4d1104
Andrew Kleindolph <akleindolph@gmail.com>
2016-08-10 22:48:11 -0700

    sound working better

--- sound_only/sound_only.ino
+++ sound_only/sound_only.ino
@@ -1,6 +1,9 @@
 #include <Adafruit_CircuitPlayground.h>   //required library to use module
 
 #define ANALOG_INPUT A4  //Analog 4 is connected to sound sensor/microphone
+const int sampleWindow = 1000; // Sample window for mic.
+unsigned int sample;
+
 
 void setup() {
   CircuitPlayground.begin();     // Setup Circuit Playground library.
@@ -9,12 +12,31 @@ void setup() {
 }
 
 void loop() {
-  
+
   // Get the sensor sensor value and print it to Serial Monitor
-  uint16_t value = analogRead(ANALOG_INPUT);
-  Serial.print("raw soundlevel= ");
-  Serial.println(value, DEC);
+  unsigned long startMillis = millis(); // Start of sample window
+  unsigned int peakToPeak = 0;   // peak-to-peak level
 
-  delay(500); // half second delay between readings
+  unsigned int signalMax = 0;
+  unsigned int signalMin = 1024;
 
+  while (millis() - startMillis < sampleWindow)
+  {
+    sample = analogRead(A4);
+    if (sample < 1024)  // toss out spurious readings
+    {
+      if (sample > signalMax)
+      {
+        signalMax = sample;  // save just the max levels
+      }
+      else if (sample < signalMin)
+      {
+        signalMin = sample;  // save just the min levels
+      }
+    }
+  }
+  peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+
+
+  Serial.println(peakToPeak);
 }
